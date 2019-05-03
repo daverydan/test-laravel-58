3163523c59fe9d7dfbc4aaa483d44376a0da6708
Danny Avery <danny.w.avery@gmail.com>
2019-05-03 11:57:29 -0600

    Redis setup in SendOrderEmail.php

--- app/Jobs/SendOrderEmail.php
+++ app/Jobs/SendOrderEmail.php
@@ -7,6 +7,7 @@ use App\Mail\OrderShipped;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -35,8 +36,20 @@ class SendOrderEmail implements ShouldQueue
      */
     public function handle()
     {
-        $recipient = 'danny@example.com';
+        /*$recipient = 'danny@example.com';
         Mail::to($recipient)->send(new OrderShipped($this->order));
-        Log::info('Emailed order ' . $this->order->id);
+        Log::info('Emailed order ' . $this->order->id);*/
+
+        // Allow only 2 emails every 1 second
+        Redis::throttle('my-mailtrap')->allow(2)->every(1)->then(function () {
+
+            $recipient = 'danny@example.com';
+            Mail::to($recipient)->send(new OrderShipped($this->order));
+            Log::info('Emailed order ' . $this->order->id);
+
+        }, function () {
+            // Could not obtain lock; this job will be re-queued
+            return $this->release(2);
+        });
     }
 }
