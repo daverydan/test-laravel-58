097ef8190eed81bfdfbcddc659b6f6b485a459e0
Danny Avery <danny.w.avery@gmail.com>
2019-05-03 11:28:43 -0600

    Setup queues

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,21 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendOrderEmail;
 use App\Order;
 use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
     public function index() {
 
-        $order = Order::findOrFail( rand(1,50) );
+        /*$order = Order::findOrFail( rand(1,50) );
 
         $recipient = 'steven@example.com';
 
         Mail::to($recipient)->send(new OrderShipped($order));
 
-        return 'Sent order ' . $order->id;
+        return 'Sent order ' . $order->id;*/
+
+        $order = Order::findOrFail( rand(1,50) );
+        SendOrderEmail::dispatch($order);
+
+        Log::info('Dispatched order ' . $order->id);
+        return 'Dispatched order ' . $order->id;
     }
 }
---
+++ app/Jobs/SendOrderEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Order;
+use App\Mail\OrderShipped;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendOrderEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $recipient = 'danny@example.com';
+        Mail::to($recipient)->send(new OrderShipped($this->order));
+        Log::info('Emailed order ' . $this->order->id);
+    }
+}
